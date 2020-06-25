bc99a231b5fa9296dd90a9265a78234bc1f64a24
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-25 01:58:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,12 @@
 def reverse_each_word(sentence)
+  
+  
   array= sentence.split(" ")
-  array2= []
-  array.each do |word|
-    array2 << word.reverse 
+  
+  array.map do |word|
+    word.reverse 
   end
-  array2.join(" ")
+  array
 end
 
 
